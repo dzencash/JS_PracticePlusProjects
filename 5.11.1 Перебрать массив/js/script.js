d9b246683d7d4ca3293ec56e997bb192a8f824b3
Dmitriy <basharkindima@gmail.com>
2019-04-18 19:10:31 +0800

    for (in) plus for (of)
    My aim to return names of "birds&animals"

---
+++ 5.11.1 Перебрать массив/js/script.js
@@ -0,0 +1,35 @@
+console.log('You are at '+window.location);
+
+let names = [
+[
+['куры', 'гуси', 'павлины'],
+['сокол', 'орел', 'соловей']
+],
+[
+['собака', 'кошка'],
+['обезьяна', 'рысь']
+]
+];
+
+let first = names[0];
+let second = names[1];
+
+// for(key of names[0]){
+// 	console.log(key)
+// }
+
+//sample below works
+// for(key of first){
+// 	console.log(key);
+// }
+// for(key in second){
+// 	console.log(second[key]);
+// }
+//sample above works
+
+for(key of names[0]){
+	console.log(key);
+}
+for(key in names[1]){
+	console.log(names[1][key]);
+}
