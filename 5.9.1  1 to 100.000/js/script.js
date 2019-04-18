8da8d95af716e5d877cb77c601ab5438a388c477
Dmitriy <basharkindima@gmail.com>
2019-04-18 11:25:41 +0800

    5.9 practice
    So the case is to select all numbers that are divisible by 6 with no reminder.
    This mady by % operator.
    De facto I have two similar ways to do that with while() and for() loop.

---
+++ 5.9.1  1 to 100.000/js/script.js
@@ -0,0 +1,21 @@
+console.log('You are at '+window.location);
+
+
+//Using for()
+
+for(let i = 1; i<100000; i++) {
+	if((i%6)==0){
+		console.log(i);
+	}
+}
+
+
+//Using while()
+// let counter = 1;
+// while(counter<100000) {
+// 	if((counter%6)==0){
+// 		console.log(counter);
+// 	}
+// 	counter++;
+	
+// }
