894909e4c3248fcb0a585abd04e5cf7eca148940
Dmitriy <basharkindima@gmail.com>
2019-04-19 15:38:24 +0800

    update v2

--- 5.11.1 Перебрать массив/js/script.js
+++ 5.11.1 Перебрать массив/js/script.js
@@ -11,14 +11,35 @@ let names = [
 ]
 ];
 
-let first = names[0];
-let second = names[1];
 
-// for(key of names[0]){
-// 	console.log(key)
+// for(let i = 0; i<names[0][0].length+names[0][1].length+names[1][0].length+names[1][1].length, i++){
+
 // }
 
+for(let i = 0; i<(names[0].length+names[1].length); i++){
+	for(key of names[0][i]){
+		console.log(key);
+	}
+	for(key of names[1][i]){
+		console.log(key);
+	}
+}
+
+// for(let i = 0; i<(names.length); i++){
+// 	for(key of names[0][i]){
+// 		console.log(key);
+// 	}
+// 	for(key of names[1][i]){
+// 		console.log(key);
+// 	}
+// }
+
+
+
 //sample below works
+// let first = names[0];
+// let second = names[1];
+
 // for(key of first){
 // 	console.log(key);
 // }
